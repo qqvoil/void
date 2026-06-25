b525a0f681fa281c150027024685358e41c6c784
qqvoil <voilthuman@gmail.com>
2026-06-25 08:56:56 +0300

    Fix: add blueprint prefixes to url_for in templates and fix app.root_path

--- routes/main.py
+++ routes/main.py
@@ -12,7 +12,7 @@ def privacy():
 
 @main_bp.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static', 'img'),
+    return send_from_directory(os.path.join(current_app.root_path, 'static', 'img'),
                                'favicon.jpg', mimetype='image/jpeg')
 
 @main_bp.route("/")
